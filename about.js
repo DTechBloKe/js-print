217cdb26c3a55db7c2d13d8b2abaa9e5f0c1eb7f
Ogunleye Kolapo <ogunleyekolapo50@gmail.com>
2022-06-24 09:49:18 +0100

    first commit

---
+++ about.js
@@ -0,0 +1,9 @@
+const about_me = {
+  name: "Oladeni ifeoluwa",
+  height: "5.9 inches",
+  country: "Nigeria",
+};
+
+alert(
+  `My name is ${about_me.name} , i am ${aboutme_height} tall and i  am from ${about_me.country}.`
+);
